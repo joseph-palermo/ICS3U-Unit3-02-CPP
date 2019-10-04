328349cb69eea8ceebe01fbc4efeaa7e9da969b3
Ubuntu <ubuntu@ip-172-31-31-42.ec2.internal>
2019-10-04 18:00:42 +0000

    Initial commit

---
+++ number_game.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2019 Joseph Palermo All rights reserved
+//
+// Created by: Joseph Palermo
+// Created on: October 2019
+// This program asks the user to guess a number
+
+#include <iostream>
+
+
+int main() {
+    // This program asks the user to guess a number
+
+    // constants
+    const int CORRECT_NUMBER = 5;
+    int userGuess;
+
+    // input
+    std::cout << "Guess the number between 0 & 9 (integer): " << std::endl;
+    std::cin >> userGuess;
+    std::cout << "" << std::endl;
+    // process
+    if (userGuess == CORRECT_NUMBER) {
+        std::cout << "Correct!" << std::endl;
+    }
+}
